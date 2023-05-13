210596da46eef404be9774e70db41ecc61af1858
Lisi4ka59 <122356446+Lisi4ka59@users.noreply.github.com>
2023-05-13 14:26:57 +0300

    new

--- src/main/java/com/lisi4ka/utils/CityReader.java
+++ src/main/java/com/lisi4ka/utils/CityReader.java
@@ -6,7 +6,7 @@ import static com.lisi4ka.utils.Checker.*;
 
 public class CityReader {
     public static String inputName() {
-        return inputStringNotNull("City name (if entered string has \" \", they will be replaced with \"_\"): ").replace(" ", "_");
+        return inputStringNotNull("City name: ").replace(" ", "**");
     }
     public static Coordinates inputCoordinates() {
         double x;
